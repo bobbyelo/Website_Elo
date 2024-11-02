243fd4aa2758ec9097675ce3b53bdce5bc30816b
bobbyelo <141641390+bobbyelo@users.noreply.github.com>
2024-11-02 23:00:15 +0100

    Update app.py

--- Elo/app.py
+++ Elo/app.py
@@ -18,4 +18,5 @@ def blog():
     return render_template('blog.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
