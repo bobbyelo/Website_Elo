86e169e5c6919a37f3ab268f1f5ebb9f2cb9eb44
bobbyelo <141641390+bobbyelo@users.noreply.github.com>
2025-02-14 23:06:27 +0100

    d

--- Elo/app.py
+++ Elo/app.py
@@ -1,23 +1,5 @@
-from flask import Flask, render_template
-import os
+from flask import render_template
 
-app = Flask(__name__)
-
-# Route for the homepage
 @app.route('/')
 def homepage():
-    return render_template('/index.html')
-
-# Route for the happy birthday page
-@app.route('/happy_birthday')
-def happy_birthday():
-    return render_template('happy_birthday.html')
-
-# Route for the blog page
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    return render_template('Index.html')
